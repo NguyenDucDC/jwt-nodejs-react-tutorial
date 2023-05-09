c7c76155b71a7528b1c73b507e243e67ba37648c
Nguyễn Văn Đức <nguyenvanduc@MacBook-Air-cua-Nguyen-4.local>
2023-05-09 23:47:16 +0700

    MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,18 +1,13 @@
 import express from "express";
+import homeController from "../controller/homeController";
 
 const router = express.Router();
 
-/**
- * 
- * @param {*} app : express app
- */
-
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello world');
-    })
+    router.get('/', homeController.handleHelloworld);
+    router.get('/user', homeController.handleUserpage);
 
-    return app.use('/', router); 
+    return app.use('/', router);
 }
 
-export default  initWebRoutes;
+export default initWebRoutes;
